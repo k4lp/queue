96857b27781efbd5b67c0617e455d1c67276202a
k4lp <kalppariya1234@gmail.com>
2020-12-15 17:22:35 +0530

    done

--- queue-circular/kalp.c
+++ queue-circular/kalp.c
@@ -18,23 +18,35 @@ void initialize() {
 }
 
 int isempty() {
+	if(pu==NULL){
+		printf("\nYou need to initialize the queue first.");
+		return '$';
+	}
 	return pu->front == pu->rear;
 }
 
 int isfull() {
+	if(pu == NULL) {
+		printf("\nYou need to initialize the queue first.");
+		return '$';
+	}
 	return ( (pu->rear +1 ) % pu->size == pu->front);
 }
 
 int dequeue() {
-	if(isempty())
+	if(isempty()){
+		printf("\nQueue is empty. Enqueue some elements first.");
 		return '$';
+	}
 	else
 		return pu->q[pu->front = (pu->front +1)% pu->size];
 }
 
 void enqueue(int x) {
-	if(isfull())
+	if(isfull()) {
+		printf("\nQueue is full. Try dequeuing some elements.\n");
 		return ;
+	}
 	else {
 		pu->q[pu->rear = (pu->rear +1) % pu->size] = x;
 	}
@@ -42,7 +54,7 @@ void enqueue(int x) {
 
 void display() {
 	if(isempty()) {
-		printf("\nQueue is empty.");
+		printf("\nQueue is empty.\n");
 		return;
 	}
 	else {
@@ -53,14 +65,38 @@ void display() {
 }
 
 int main() {
-	initialize();
-	enqueue(3);
-	display();
-	enqueue(3344);
-	display();
-	dequeue();
-	display();
-	dequeue();
-	display();
+	int choice = 0;
+	do {
+		printf("\n1.Enqueue.\n2.Dequeue\n3.Display\n4.Initialize.\n5.Exit \nEnter your choice :- ");
+		scanf(" %d",&choice);
+		switch(choice) {
+		case 1 :
+			printf("\nEnter the number to add :- ");
+			int x = 0;
+			scanf(" %d",&x);
+			enqueue(x);
+			break;
+		case 2 :
+			if(isempty()) {
+				printf("\nQueue is empty.\n");
+				break;
+			}
+			x = dequeue();
+			printf("\n %d is removed from the queue.\n",x);
+			break;
+		case 3 :
+			display();
+			break;
+		case 4 :
+			initialize();
+			break;
+		case 5 :
+			printf("\nExiting...");
+			break;
+		default :
+			printf("\nWrong choice.");
+			break;
+		}
+	} while(choice != 5);
 	return 0;
 }
