7a6c9870f9d30d56cf6634cef02c47f22f3565e6
k4lp <kalppariya1234@gmail.com>
2020-12-16 00:50:54 +0530

    done

--- linkedlistdoubly/kalp.c
+++ linkedlistdoubly/kalp.c
@@ -56,7 +56,6 @@ void addlast() {
 		node *tmp = (node *)malloc(sizeof(node));
 		printf("\nEnter the element :- ");
 		scanf(" %d",&tmp->data);
-		node *ptr = head;
 		tail->next = tmp;
 		tmp->next = NULL;
 		tmp->prev = tail;
@@ -81,15 +80,15 @@ void addpos() {
 		node *ptr = head;
 		pos --;
 		while((pos > 1) && (ptr->next != NULL)) {
-			ptr->next;
+			ptr = ptr->next;
 			pos--;
 		}
-		if(pos > 2) {
+		if(pos > 1) {
 			printf("\nPosition number is out of bound.\nEntering as a last element.");
 			addlast();
 			return;
 		}
-		else if(pos > 1) {
+		else if(ptr->next == NULL) {
 			addlast();
 			return;
 		}
@@ -110,6 +109,11 @@ void delfirst() {
 		return;
 	}
 	else {
+		if(head!=NULL && (head == tail)) {
+			free(head);
+			head=tail=NULL;
+			return;
+		}
 		head = head ->next;
 		free(head->prev);
 		head->prev = NULL;
@@ -125,5 +129,79 @@ void dellast() {
 		tail = tail->prev;
 		free(tail->next);
 		tail->next = NULL;
+	}
+}
+
+void delpos() {
+	if(isempty()) {
+		printf("\nList is empty.");
+		return;
+	}
+	else {
+		printf("\nEnter the position :- ");
+		int pos = 0;
+		scanf(" %d",&pos);
+		if(pos == 1) {
+			delfirst();
+			return;
+		}
+		node *ptr = head;
+		while((pos > 1) && (ptr->next != NULL)) {
+			pos--;
+			ptr=ptr->next;
+		}
+		if( pos > 1){
+			printf("\nPosition out of bound. Deleting last element.");
+			dellast();
+			return;
+		}
+		else if(ptr->next == NULL) {
+			dellast();
+			return;
+		}
+		node *ptr1 = ptr->prev,*ptr2 = ptr->next;
+		free(ptr);
+		ptr1->next = ptr2;
+		ptr2->prev = ptr1;
+	}
+}
+
+void display() {
+	if(isempty()) {
+		printf("\nList is empty.");
+		return;
+	}
+	else {
+		node *ptr = head;
+		while(ptr!=NULL) {
+			printf("\n %d",ptr->data);
+			ptr=ptr->next;
+		}
+	}
+}
+
+int main() {
+	int choice = 0;
+	while(choice!=9) {
+		printf("\n1.Create.\n2.Add first\n3.Add last.\n4.Add position\n5.Del first\n6.Del last\n7.Del Pos\n8.Display\n9.Exit\nEnteer your choice :- ");
+		scanf(" %d",&choice);
+		switch(choice) {
+		case 1 :
+			create();
+			break;
+		case 2: addfirst();break;
+		case 3: addlast();break;
+		case 4: addpos();break;
+		case 5: delfirst();break;
+		case 6: dellast();break;
+		case 7: delpos();break;
+		case 8: display();break;
+		case 9:
+			printf("\nExiting....");
+			break;
+		default :
+			printf("\nWrong choice.");
+			break;
+		}
 	}
 }
