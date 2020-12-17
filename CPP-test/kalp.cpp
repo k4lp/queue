2d125ad1a430fc7a33fe993fef17a22b479bbcb4
k4lp <kalppariya1234@gmail.com>
2020-12-18 01:38:41 +0530

    done

--- CPP-test/kalp.cpp
+++ CPP-test/kalp.cpp
@@ -1,8 +1,10 @@
-/*
- * kalp.cpp
- *
- *  Created on: 17-Dec-2020
- *      Author: kalp
- */
+#include<iostream>
+using namespace std;
 
-
+int main() {
+	char *s;
+	s = new char[20];
+	cin.get(s,20);
+	cout<<"Hello"<<endl<<s<<endl;
+	return 0;
+}
