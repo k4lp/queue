d8647196bb02fba98c1e778b8ac15b0cb4d55ecd
k4lp <kalppariya1234@gmail.com>
2020-12-30 22:59:02 +0530

    done

--- CPP-test/kalp.cpp
+++ CPP-test/kalp.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+double mypow(int base,int exponent) {
+	int result = 1;
+	while(exponent >= 1) {
+		result = result * base;
+		exponent --;
+	}
+	return result;
+}
+
+int main() {
+	string firstname , lastname ;
+	cout<<"Enter the first name :- "<<endl;
+	getline(cin,firstname);
+	cout<<"Enter the last name :-"<<endl;
+	getline(cin,lastname);
+	string name = firstname + " " + lastname + ".";
+	cout<<"So your name is "<<name<<endl;
+
+	cout<<"Your name length is :- " << name.length()<<endl;
+	return 0;
+}
 
